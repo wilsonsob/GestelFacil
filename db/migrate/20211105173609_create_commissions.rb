c1892745e82116b70bb6729fff64486bb90ee667
Wilson Filho <wilsonsenhor@gmail.com>
2021-11-05 15:14:59 -0300

    added default true

--- db/migrate/20211105173609_create_commissions.rb
+++ db/migrate/20211105173609_create_commissions.rb
@@ -2,7 +2,7 @@ class CreateCommissions < ActiveRecord::Migration[6.0]
   def change
     create_table :commissions do |t|
       t.text :note
-      t.boolean :active
+      t.boolean :active, default: true
       t.references :contract, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
