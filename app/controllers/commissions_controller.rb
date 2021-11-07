39b4e1692573e8b4d34366e8dbd5dd98b99c321e
Luanda Leitao <luanda.leitao@gmail.com>
2021-11-07 13:00:35 -0300

    ajuste controllers commissions e contracts e das views index e show de contracts - correcao select de commissions

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -5,11 +5,10 @@ before_action :set_contract, only: %i[new create]
     @commission = Commission.new
   end
 
+  # Vinculo de fiscal via contract#show
   def create
-    @contract = Contract.find(params[:contract_id])
     @commission = Commission.new(commission_params)
     @commission.contract_id = @contract.id
-    # @commission.user_id = @user.id
     if @commission.save!
       redirect_to @contract, notice: 'Fiscal vinculado com sucesso.'
     else
@@ -26,5 +25,4 @@ before_action :set_contract, only: %i[new create]
   def set_contract
     @contract = Contract.find(params[:contract_id])
   end
-
 end
