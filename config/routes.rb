ac696b19a24595256b9a5fe615ca4ad2ac08e43b
Wilson Filho <wilsonsenhor@gmail.com>
2021-11-07 11:05:50 -0300

    criado metodos new e create dos termos e deslocado os forms para pasta shared

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :contracts, except: :destroy do
     resources :commissions, only: %i[create]
+    resources :terms, except: :destroy
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
