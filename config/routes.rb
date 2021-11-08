c80cf291881386ba5fa27a79277fc2c90a777d24
Luanda Leitao <luanda.leitao@gmail.com>
2021-11-08 15:08:35 -0300

    bug da edit terms resolvido

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :contracts, except: :destroy do
     resources :commissions, only: %i[create]
-    resources :terms, except: :destroy
+    resources :terms, only: %i[new create]
   end
+  resources :terms, only: %i[edit update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
