0ebebd5a6213570b21fd7982af5ebbc8ff9b072a
Eugenia Brandao <eugeniacb@gmail.com>
2021-11-09 00:19:14 -0300

    seed commission updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,19 @@ user5 = User.create!(full_name: 'Eugenia User', email: 'eugenia@teste.com', pass
 user6 = User.create!(full_name: 'Wilson User', email: 'wilson@teste.com', password: '123456', admin: false)
 puts "common profiles: Eugenia, Luanda e Wilson - done!!!"
 
+# Membros da Comissão da ANVISA
+user7 = User.create!(full_name: 'Bruno Rodrigues', email: 'bruno@teste.com', password: '123456', admin: false)
+user8 = User.create!(full_name: 'Paulo Alencar', email: 'paulo@teste.com', password: '123456', admin: false)
+
+# Membros da Comissão da SUDENE
+user9 = User.create!(full_name: 'Alexandre Silva', email: 'alexandre@teste.com', password: '123456', admin: false)
+user10 = User.create!(full_name: 'Maria Fernandes', email: 'maria@teste.com', password: '123456', admin: false)
+
+# Membros da Comissão do COMAER
+user11 = User.create!(full_name: 'Daniel Sales', email: 'daniel@teste.com', password: '123456', admin: false)
+user12 = User.create!(full_name: 'Tiago Batista', email: 'tiago@teste.com', password: '123456', admin: false)
+puts "Members of the commission created - done!!!"
+
 contract1 = Contract.create!(number: '20/SUDENE/2019', cnpj_contractor: '09263130000191', cnpj_hired: '40432544000147', company: 'CLARO S.A.',
                              object: "Contratação de empresa prestadora de Sistema Móvel Celular (SMC), na modalidade Local e
                              Longa Distância Nacional (LDN) a serem utilizados pela SUDENE na localidade de Aracaju através de 04
@@ -81,6 +94,137 @@ contract10 = Contract.create!(number: '10/COMAER/2017', cnpj_contractor: '003944
                               até o limite de 60 (sessenta) meses.")
 
 puts "Contracts - done!!!"
+
+# Comissões da SUDENE
+commission1 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                 contract_id: contract1.id, user_id: user3.id)
+
+commission2 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                 contract_id: contract1.id, user_id: user6.id)
+
+commission3 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                 contract_id: contract1.id, user_id: user9.id)
+
+commission4 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                 contract_id: contract1.id, user_id: user10.id)
+
+commission5 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                 contract_id: contract6.id, user_id: user3.id)
+
+commission6 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                 contract_id: contract6.id, user_id: user6.id)
+
+commission7 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                 contract_id: contract6.id, user_id: user9.id)
+
+commission8 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                 contract_id: contract6.id, user_id: user10.id)
+
+commission9 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                 contract_id: contract7.id, user_id: user3.id)
+
+commission10 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                  contract_id: contract7.id, user_id: user6.id)
+
+commission11 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                  contract_id: contract7.id, user_id: user9.id)
+
+commission12 = Commission.create!(note: 'Portaria SUDENE N°30, de 15 de outubro de 2021.',
+                                  contract_id: contract7.id, user_id: user10.id)
+
+puts "SUDENE commissions - done!"
+
+# Comissões da ANVISA
+commission13 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract3.id, user_id: user1.id)
+
+commission14 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract3.id, user_id: user4.id)
+
+commission15 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract3.id, user_id: user7.id)
+
+commission16 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract3.id, user_id: user8.id)
+
+commission17 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract8.id, user_id: user1.id)
+
+commission18 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract8.id, user_id: user4.id)
+
+commission19 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract8.id, user_id: user7.id)
+
+commission20 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract8.id, user_id: user8.id)
+
+commission21 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract9.id, user_id: user1.id)
+
+commission22 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract9.id, user_id: user4.id)
+
+commission23 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract9.id, user_id: user7.id)
+
+commission24 = Commission.create!(note: 'Portaria ANVISA N° 12, de 4 de fevereiro de 2021.',
+                                  contract_id: contract9.id, user_id: user8.id)
+
+puts "ANVISA commissions - done!"
+
+# Comissões do COMAER
+commission25 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract2.id, user_id: user2.id)
+
+commission26 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract2.id, user_id: user5.id)
+
+commission27 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract2.id, user_id: user11.id)
+
+commission28 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract2.id, user_id: user12.id)
+
+commission29 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract4.id, user_id: user2.id)
+
+commission30 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract4.id, user_id: user5.id)
+
+commission31 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract4.id, user_id: user11.id)
+
+commission32 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract4.id, user_id: user12.id)
+
+commission33 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract5.id, user_id: user2.id)
+
+commission34 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract5.id, user_id: user5.id)
+
+commission35 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract5.id, user_id: user11.id)
+
+commission36 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract5.id, user_id: user12.id)
+
+commission37 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract10.id, user_id: user2.id)
+
+commission38 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract10.id, user_id: user5.id)
+
+commission39 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract10.id, user_id: user11.id)
+
+commission40 = Commission.create!(note: 'Portaria COMAER N° 1, de 6 de janeiro de 2021.',
+                                  contract_id: contract10.id, user_id: user12.id)
+
+puts "COMAER commissions - done!"
+
+puts "Commissions - done!!!"
 
 term1 = Term.create!(number: 'Contrato N° 20/SUDENE/2019', date_start: '21/08/2019', date_end: '20/08/2020', description: 'Primeiro ano do contrato.', contract_id: contract1.id)
 term2 = Term.create!(number: 'Termo Aditivo N° 01', date_start: '21/08/2020', date_end: '20/08/2021', description: 'Segundo ano do contrato.', contract_id: contract1.id)
