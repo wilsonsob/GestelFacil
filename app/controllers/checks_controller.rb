52e5576a66506d5bd74ba534c2554b7c3275effd
Luanda Leitao <luanda.leitao@gmail.com>
2021-11-11 10:18:20 -0300

    ajustes pagina de analysis, conferencia de data implementada, nova rota para pagina do usuario

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,29 +1,58 @@
 class ChecksController < ApplicationController
 
   def analysis
-    # https://guides.rubyonrails.org/active_record_querying.html#eager-loading-associations
-    # @invoices = Invoice.all
-    # @invoices = Invoice.all.limit(5) # funciona!
-    # @invoices = Invoice.where(service_code: '91801') # funciona!
-    # @invoices = Invoice.all.find_by_number!(:number) # nao funciona
-    # @invoices = Invoice.all.where(number: @invoice.number) # nao funciona
-    # @invoices = Invoice.select(:number).map(&:number) # nao funciona para trazer as instancias, mas listou todos os numeros de fatura
-    # @invoices.uniq.count nao quebra mas nao serve
-    @invoices = Invoice.all
-    @invoices_uniq = @invoices.uniq(&:number)
-    @list_invoice_number = []
-    @invoices_uniq.each do |invoice|
-      @list_invoice_number << Invoice.find_by(number: invoice.number)
-    end
-    @list_invoice_number.sort_by {|i| i.created_at}
+    # Lista de faturas de numero exclusivo:
+    @invoices_uniq_number = Invoice.all.uniq(&:number)
+    # @invoices_uniq_number.count = qtd de faturas
+    # @invoices_uniq_number.class => array
+
+    # Lista de faturas de cnpj (do contratante) exclusivo
+    @invoices_uniq_cnpj = Invoice.all.uniq{ |invoice| [invoice.number, invoice.cnpj_contractor] }
+
+    # Para analises relativas ao current_user
+    @user = current_user
+    @my_commissions = Commission.where(user_id: @user)
+
+    
+
+
+
+
+
+    # Invoice.find_by(number: invoice.number, service_code: invoice.cnpj_contractor)
+    # @invoices_uniq_cnpj = @list_invoice_number.sort_by {|i| i.created_at}
+
+
+
+
+
+
+
+
+
+
+
   end
 
   # def list_invoice_number
+  #   @invoices = Invoice.all
+  #   @invoices_uniq = @invoices.uniq(&:number)
   #   @list_invoice_number = []
   #   @invoices_uniq.each do |invoice|
-  #     @list_invoice_number << Invoice.find_by_(number: invoice.number)
+  #     @list_invoice_number << Invoice.find_by(number: invoice.number)
   #   end
   #   @list_invoice_number.sort_by {|i| i.created_at}
+  # end
+
+  # def list_by_service_code
+  #   @invoices = Invoice.all
+  #   # @invoices_uniq = @invoices.uniq(&:service_code)
+  #   @invoices_uniq = @invoices.uniq{ |invoice| [invoice.number, invoice.service_code] }
+  #   @list_by_service_code = []
+  #   @invoices_uniq.each do |invoice|
+  #     @list_by_service_code << Invoice.find_by(number: invoice.number && service_code: invoice.service_code)
+  #   end
+  #   @list_by_service_code.sort_by {|i| i.created_at}
   # end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,10 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def profile
+    @user = current_user
+    @my_commissions = Commission.where(user_id: @user)
+    @invoices_uniq_number = Invoice.all.uniq(&:number)
+  end
 end
