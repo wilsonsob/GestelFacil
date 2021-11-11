709379709948d0642dfff2328557e81acb89fe87
Luanda Leitao <luanda.leitao@gmail.com>
2021-11-11 12:48:15 -0300

    correcao-each-da-view-profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,6 @@ class PagesController < ApplicationController
     @user = current_user
     @my_commissions = Commission.where(user_id: @user)
     @invoices_uniq_number = Invoice.all.uniq(&:number)
+    @invoices_uniq_cnpj = Invoice.all.uniq{ |invoice| [invoice.number, invoice.cnpj_contractor] }
   end
 end
