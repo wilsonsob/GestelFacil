17ca476f41f56aa6f5fdb9efdbfaf6afe66dd62b
Eugenia Brandao <eugeniacb@gmail.com>
2021-11-12 12:27:18 -0300

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,46 +10,46 @@ User.destroy_all
 
 puts "destroy done!!!"
 
-user1 = User.create!(full_name: 'Luanda Admin', email: 'luanda@admin.com', password: '123456', admin: true)
-user2 = User.create!(full_name: 'Eugenia Admin', email: 'eugenia@admin.com', password: '123456', admin: true)
-user3 = User.create!(full_name: 'Wilson Admin', email: 'wilson@admin.com', password: '123456', admin: true)
+user1 = User.create!(full_name: 'Luanda Leitão', email: 'luanda@admin.com', password: '123456', admin: true)
+user2 = User.create!(full_name: 'Eugenia Brandão', email: 'eugenia@admin.com', password: '123456', admin: true)
+user3 = User.create!(full_name: 'Wilson Filho', email: 'wilson@admin.com', password: '123456', admin: true)
 puts "admins (gestor de contrato): Eugenia, Luanda e Wilson - done!!!"
 
-user4 = User.create!(full_name: 'Luanda User', email: 'luanda@teste.com', password: '123456', admin: false)
-user5 = User.create!(full_name: 'Eugenia User', email: 'eugenia@teste.com', password: '123456', admin: false)
-user6 = User.create!(full_name: 'Wilson User', email: 'wilson@teste.com', password: '123456', admin: false)
-puts "common profiles: Eugenia, Luanda e Wilson - done!!!"
+# user4 = User.create!(full_name: 'Luanda Leitão', email: 'luanda@teste.com', password: '123456', admin: false)
+# user5 = User.create!(full_name: 'Eugenia Brandão', email: 'eugenia@teste.com', password: '123456', admin: false)
+# user6 = User.create!(full_name: 'Wilson Filho', email: 'wilson@teste.com', password: '123456', admin: false)
+# puts "common profiles: Eugenia, Luanda e Wilson - done!!!"
 
-contract1 = Contract.create!(number: '030/BARF - CINDACTA III/2020-2012310', cnpj_contractor: '00394429009329', cnpj_hired: '76535764000143', company: 'OI S.A.',
-                             object: "Contratação de empresa prestadora de Serviço Telefônico Fixo Comutado (STFC), na modalidade Local e
-                             Longa Distância Nacional (LDN) a serem utilizados pelo Destacamento de Controle do Espaço Aéreo de Natal através de 06
-                             (seis) linhas diretas analógicas não residenciais (NRES), pelo período de 12 (doze) meses, podendo ser
-                             prorrogado por sucessivos períodos, até o limite de 60 (sessenta) meses.")
+contract1 = Contract.create!(number: '17/COMAER/2020', cnpj_contractor: '00394429009329', cnpj_hired: '76535764000143', company: 'OI S.A.',
+                             object: "Serviço Telefônico Fixo Comutado (STFC), na modalidade Local e
+                                      Longa Distância Nacional (LDN), pelo período de 12 (doze) meses, podendo ser
+                                      prorrogado por sucessivos períodos, até o limite de 60 (sessenta) meses.")
+
+contract2 = Contract.create!(number: '39/COMAER/2021', cnpj_contractor: '00394429009329', cnpj_hired: '40432544000147', company: 'CLARO S.A.',
+                             object: "Serviço Telefônico Fixo Comutado (STFC), na modalidade Local, Longa Distância
+                                      Nacional (LDN) e Longa Distância Internacional (LDI), pelo período de 12 (doze) meses,
+                                      podendo ser prorrogado por sucessivos períodos, até o limite de 60 (sessenta) meses.")
+
+contract3 = Contract.create!(number: '10/COMAER/2019', cnpj_contractor: '00394429009329', cnpj_hired: '02449992005638', company: 'VIVO S.A.',
+                             object: "Serviço Móvel Celular (SMC), na modalidade Local, Longa Distância Nacional (LDN) e
+                                      Longa Distância Internacional (LDI), pelo período de 12 (doze) meses, podendo ser
+                                      prorrogado por sucessivos períodos, até o limite de 60 (sessenta) meses.")
 
 puts "Contracts - done!!!"
 
 # Comissões do COMAER
-commision1 = Commission.create!(note: 'Portaria CINDACTA III N° 138/AIAC, de 21 de outubro de 2021. Publicado no Boletim Interno Ostensivo N° 201, de 25/10/2021.',
+commision1 = Commission.create!(note: 'Portaria N° 135/AICT, de 20 de janeiro 2021. Publicado no Boletim Interno Ostensivo N° 15, de 25/01/2021.',
                                   contract_id: contract1.id, user_id: user1.id)
 
-commision2 = Commission.create!(note: 'Portaria CINDACTA III N° 138/AIAC, de 21 de outubro de 2021. Publicado no Boletim Interno Ostensivo N° 201, de 25/10/2021.',
+commision2 = Commission.create!(note: 'Portaria N° 135/AICT, de 20 de janeiro 2021. Publicado no Boletim Interno Ostensivo N° 15, de 25/01/2021.',
                                   contract_id: contract1.id, user_id: user2.id)
 
-commision3 = Commission.create!(note: 'Portaria CINDACTA III N° 138/AIAC, de 21 de outubro de 2021. Publicado no Boletim Interno Ostensivo N° 201, de 25/10/2021.',
+commision3 = Commission.create!(note: 'Portaria N° 135/AICT, de 20 de janeiro 2021. Publicado no Boletim Interno Ostensivo N° 15, de 25/01/2021.',
                                   contract_id: contract1.id, user_id: user3.id)
-
-commision4 = Commission.create!(note: 'Portaria CINDACTA III N° 138/AIAC, de 21 de outubro de 2021. Publicado no Boletim Interno Ostensivo N° 201, de 25/10/2021.',
-                                  contract_id: contract1.id, user_id: user4.id)
-
-commision5 = Commission.create!(note: 'Portaria CINDACTA III N° 138/AIAC, de 21 de outubro de 2021. Publicado no Boletim Interno Ostensivo N° 201, de 25/10/2021.',
-                                  contract_id: contract1.id, user_id: user5.id)
-
-commision6 = Commission.create!(note: 'Portaria CINDACTA III N° 138/AIAC, de 21 de outubro de 2021. Publicado no Boletim Interno Ostensivo N° 201, de 25/10/2021.',
-                                  contract_id: contract1.id, user_id: user6.id)
 
 puts "Commissions - done!!!"
 
-term1 = Term.create!(number: '030/BARF - CINDACTA III/2020-2012310', date_start: '30/12/2020', date_end: '29/12/2021', description: 'Primeiro ano do contrato.', contract_id: contract1.id)
+term1 = Term.create!(number: '17/COMAER/2020', date_start: '05/01/2021', date_end: '06/01/2022', description: 'Primeiro ano do contrato.', contract_id: contract1.id)
 puts "Terms - done!!!"
 
 item1 = Item.create!(item_number: 41, item_title: 'Assinatura de Linhas Analógicas não residenciais (NRES)',
@@ -77,6 +77,9 @@ puts "Starting seed to invoices from CSV file..."
 csv_options = { col_sep: ';', quote_char: '"', headers: :first_row }
 
 filepath = 'db/invoice.csv'
+
+# Arquivo para testar o blaze (faturas de fev a set)
+# filepath = 'db/invoice2.csv'
 
 CSV.foreach(filepath, csv_options) do |row|
   # Here, row is an array of columns
