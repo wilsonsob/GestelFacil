4cbe79a15deb8ca6fa0dccd07cdb15cf4e7fcb21
Luanda Leitao <luanda.leitao@gmail.com>
2021-11-15 14:55:57 -0300

    install-blazer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  authenticate :user, ->(user) { user.admin? } do
+  mount Blazer::Engine, at: "blazer"
+end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
